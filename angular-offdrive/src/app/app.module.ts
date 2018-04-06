3672ecb4ea2d5fc0e0c51b273f6ffe345d211de7
frozenpause <frozenpause@gmail.com>
2018-04-06 17:07:14 +0300

    Changes 6-apr

--- angular-offdrive/src/app/app.module.ts
+++ angular-offdrive/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { ItemsComponent } from './items/items.component';
     ItemsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-offdrive/src/app/items/items.component.ts
+++ angular-offdrive/src/app/items/items.component.ts
@@ -6,8 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
+  title: string;
+  desc: string;
+  sidebar: object;
+  settings: string[];
+  showSettings: boolean;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Drive',
+    this.desc = 'Office drive description',
+    this.sidebar = {
+      login: 'Login',
+      logout: 'Logout',
+      profile: 'Profile'
+    }
+    this.settings = ['edit','delete','rename'];
+    this.showSettings = false;
+  }
+  toggleSettings() {
+    if (this.showSettings == true) {
+      this.showSettings = false;
+    } else {
+      this.showSettings = true;
+    }
+  }
 
   ngOnInit() {
   }
